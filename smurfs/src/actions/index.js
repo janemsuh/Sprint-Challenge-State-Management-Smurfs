029130e8f7ef3626a3977d02e201225691a09c74
Jane Suh <sourcherryyogurt@gmail.com>
2020-01-12 20:07:27 -0500

    Fetching all smurfs in the DB

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
+
+export const fetchSmurfs = () => {
+    return dispatch => {
+        dispatch({ type: FETCH_SMURFS_START });
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then(res => {
+                console.log(res);
+                dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data })
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch({ type: FETCH_SMURFS_FAIL, payload: err })
+            })
+    };
+};
